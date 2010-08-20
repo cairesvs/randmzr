e78822d784a85fec0e734a6a6b9d7923ec282435
Caires Vinicius <cairesvs@gmail.com>
2010-08-19 23:43:50 -0300

    adicionando mais logs

--- views.py
+++ views.py
@@ -6,37 +6,43 @@ def htmlSource(request):
   boards = ['sp','b','g','v','mu','lit','ic','x','co','vp']
   random.shuffle(boards)
   sock = urllib.urlopen("http://boards.4chan.org/" + boards[0])    
-  usock = urllib.urlopen('http://boards.4chan.org/b')
-  logging.debug("url %s", sock.geturl())
+  logging.info("url %s", sock.geturl())
   i = 0
-  marotao = sock.geturl() == "http://www.4chan.org/" or sock.geturl() == "http://www.4chan.org/banned"
-  quotas = usock.geturl() == "http://www.4chan.org/" or usock.geturl() == "http://www.4chan.org/banned"
-  while marotao and quotas:
+  while  sock.geturl() == "http://www.4chan.org/" or sock.geturl() == "http://www.4chan.org/banned":
     i += 1
     sock.close()
-    usock.close()
     random.shuffle(boards)
     sock = urllib.urlopen("http://boards.4chan.org/" + boards[0] + "/" + str(i))
-    usock = urllib.urlopen('htt://boards.4chan.org/b')
-    logging.debug("url dentro do while 2 %s", sock.geturl())
+    logging.info("url dentro do while 2 %s", sock.geturl())
+
+  usock = urllib.urlopen('http://boards.4chan.org/b')
+  while usock.geturl() == "http://www.4chan.org/" or usock.geturl() == "http://www.4chan.org/banned":
+    usock.close()
+    usock = urllib.urlopen('http://boards.4chan.org/b')
 
   html = sock.read()
   quotesHtml = usock.read()
   sock.close()
   allImages = re.findall('<a href="(.*images.*)" target="_blank">',html) 
   realQuotes = re.findall('<blockquote>.*',quotesHtml)
+  logging.info("images all %s", allImages)
+  logging.info("quotes real %s",realQuotes)
   realImages = []
   for image in allImages:
     realImages.append(re.search('(http://images.*)', image).group(0))
   
   random.shuffle(realImages)
   random.shuffle(realQuotes)
+  logging.info("images %s", realImages)
+  logging.info("quotes %s",realQuotes)
   return render_to_response('boardTemplate.html', {'image' : realImages[0], 'quote' : realQuotes[0]})
 
 def ajax(request):
   sock = urllib.urlopen(request.GET['url'])
   img = sock.read()
+  logging.info(img)
   sock.close()
+  logging.info(img)
   return HttpResponse(img, mimetype="image/jpg")
 
 def index(request):
